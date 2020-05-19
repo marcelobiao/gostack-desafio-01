58c453968b41949645d149b7467f4a12f232129c
Marcelo Biao <marcelobiao2@gmail.com>
2020-05-19 20:32:01 -0300

    Criando model de Tasks

---
+++ src/app/models/Tasks.js
@@ -0,0 +1,22 @@
+import Sequelize, {Model} from 'sequelize';
+
+class Tasks extends Model{
+  static init(sequelize){
+    super.init(
+      {
+        title: Sequelize.STRING
+      },
+      {
+        sequelize
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models){
+    this.belongsTo(models.Projects, {foreignKey: 'project_id'});
+  }
+}
+
+export default Tasks;
